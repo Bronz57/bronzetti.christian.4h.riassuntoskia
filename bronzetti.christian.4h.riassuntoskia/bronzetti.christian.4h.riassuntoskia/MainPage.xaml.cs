1c684c2b53a7a165211c4d1f6b2beb202cde79e9
bronz57 <{chbronz88}@gmail.com>
2021-01-15 08:48:08 +0100

    Primo commit

---
+++ bronzetti.christian.4h.riassuntoskia/bronzetti.christian.4h.riassuntoskia/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Xamarin.Forms;
+using SkiaSharp;
+
+namespace bronzetti.christian._4h.riassuntoskia
+{
+    public partial class MainPage : ContentPage
+    {
+        public int margineSinistro { get; set; } = 100;
+        public int margineSuperiore { get; set; } = 100;
+        public int larghezzaRettangolo { get; set; } = 200;
+        public int altezzaRettangolo { get; set; } = 300;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Tela(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs e)
+        {
+            var info = e.Info;
+            var canvas = e.Surface.Canvas;
+            canvas.Clear();
+
+            int larghezza = info.Rect.Width;
+            int altezza = info.Rect.Height;
+
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.BlueViolet,
+                StrokeWidth = 3
+            };
+
+            SKPath rettangolo = AreaDelRettangolo();
+
+            canvas.DrawPath(rettangolo, paint);
+
+            TelaDaDisegno.InvalidateSurface();
+        }
+
+        SKPath AreaDelRettangolo()
+        {
+            SKPath rettangolo = new SKPath();
+
+            rettangolo.MoveTo(margineSinistro, margineSuperiore);
+            rettangolo.LineTo(margineSinistro + larghezzaRettangolo, margineSuperiore);
+            rettangolo.LineTo(margineSinistro + larghezzaRettangolo, margineSuperiore + altezzaRettangolo);
+            rettangolo.LineTo(margineSinistro, margineSuperiore + altezzaRettangolo);
+            rettangolo.LineTo(margineSinistro, margineSuperiore);
+
+            return rettangolo;
+        }
+
+        private void btnDisegna_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
